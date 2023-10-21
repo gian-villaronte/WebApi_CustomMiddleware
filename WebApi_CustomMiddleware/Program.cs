37f88ae7004ef92aa264bf59c93b9d38e1fd3a33
Gian Villaronte <gianvillaronte@gmail.com>
2023-10-21 14:07:41 -0400

    page 20

--- WebApi_CustomMiddleware/Program.cs
+++ WebApi_CustomMiddleware/Program.cs
@@ -12,6 +12,19 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});
+
 app.MapControllers();
 
 app.Run();
